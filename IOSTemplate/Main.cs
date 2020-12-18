f8517e2774ea1274e376b807b0ad9231bfa5f1a5
Roman Danylevych <roma.dan2001@gmail.com>
2020-12-18 20:12:22 +0200

    Added IOS Template

---
+++ IOSTemplate/Main.cs
@@ -0,0 +1,9 @@
+﻿using UIKit;
+
+namespace IOSTemplate
+{
+    public class Application
+    {
+        private static void Main(string[] args) => UIApplication.Main(args, null, "AppDelegate");
+    }
+}
