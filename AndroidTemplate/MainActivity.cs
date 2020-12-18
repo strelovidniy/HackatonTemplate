2a6469426e8212fb629988f7423932a9c2fa301a
Roman Danylevych <roma.dan2001@gmail.com>
2020-12-18 18:57:02 +0200

    Added Android Template

---
+++ AndroidTemplate/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Webkit;
+using Debug = System.Diagnostics.Debug;
+
+namespace AndroidTemplate
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private WebView _webView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            RequestedOrientation = ScreenOrientation.Portrait;
+            
+            _webView = FindViewById<WebView>(Resource.Id.webView1);
+            _webView.SetWebViewClient(new WebViewClient());
+
+            _webView.Settings.JavaScriptEnabled = true;
+            _webView.Settings.UserAgentString = "AndroidTemplate";
+
+            _webView.LoadUrl("https://hackatontest20201208233254.azurewebsites.net/");
+        }
+
+        public override bool OnKeyDown(Keycode keyCode, KeyEvent e)
+        {
+            if (keyCode == KeyEvent.KeyCodeFromString("BACK") && _webView.CanGoBack())
+            {
+                _webView.GoBack();
+
+                return true;
+            }
+
+            Finish();
+
+            return base.OnKeyDown(keyCode, e);
+        }
+    }
+}
