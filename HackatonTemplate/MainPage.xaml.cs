73d6b0f61731631d61b3f18d04dd80fe9f5b4160
Roman Danylevych <roma.dan2001@gmail.com>
2020-12-18 18:03:56 +0200

    Add project files.

---
+++ HackatonTemplate/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.Media.SpeechSynthesis;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace DesktopTemplate
+{
+    public sealed partial class MainPage
+    {
+        public MainPage() => InitializeComponent();
+        
+        public async void Greeting()
+        {
+            var mediaElement = new MediaElement();
+            var stream = await new SpeechSynthesizer().SynthesizeTextToStreamAsync("Hello Marathone Team!");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
+
+        private void HackatonWebView_OnLoadCompleted(object sender, NavigationEventArgs e) => Greeting();
+    }
+}
